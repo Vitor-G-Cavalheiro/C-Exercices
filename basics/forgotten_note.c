4c6a691d9af67d75eff6160621bc6cc54376632d
Vitor G Cavalheiro <126813410+Vitor-G-Cavalheiro@users.noreply.github.com>
2026-04-23 14:50:12 -0300

    Create forgotten_note.c

---
+++ basics/forgotten_note.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A, M, B;
+    scanf("%d", &A);
+    scanf("%d", &M);
+    B = (M*2) - A;
+    printf("%d", B);
+	return 0;
+}
